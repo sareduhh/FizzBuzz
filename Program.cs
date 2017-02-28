63fdbe7c1c6a73ce0222311e14f78e3d6c94efbd
Sarah Kaminski <sarahe.kaminski@gmail.com>
2017-02-28 12:00:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print numbers 1-100, for each number divisible by 3 print "Fizz"
+            //for each number divisible by 5 print "Buzz"
+            //for numbers divisible by BOTH 3 AND 5 print "FizzBuzz"
+
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz");
+                else if (fizz)
+                    Console.WriteLine("Fizz");
+                else if (buzz)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+
+        
+
+        }
+    }
+}
